d23fcff63f679b2abaa5bae9a80350d29fb6975b
Aleksandr <aleksdark13@gmail.com>
2024-09-21 15:11:07 +0300

    add gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import ContentTable from "./components/ContentTable/ContentTable.tsx";
 import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client";
--- src/components/ContentTable/ContentTable.tsx
+++ src/components/ContentTable/ContentTable.tsx
@@ -15,11 +15,9 @@ import * as React from "react";
 import Modals from "../Modal/Modal";
 import {useQuery} from "@apollo/client";
 import {CircularProgress} from '@mui/material';
-import {ApolloClient, InMemoryCache, createHttpLink} from "@apollo/client";
 
 
 import {GET_CUSTOMERS} from "../../gql/query";
-import {setContext} from "@apollo/client/link/context";
 
 
 // Define types for customer data
@@ -32,48 +30,48 @@ export interface Customer {
     label: string;
 }
 
-const customers: Customer[] = [
-    {
-        id: 1,
-        name: "Lillian Carter",
-        email: "xcollier@goodwin.com",
-        phone: "+1-267-551-8666",
-        company: "Larkin Group",
-        label: "Marketing",
-    },
-    {
-        id: 2,
-        name: "Otto Walker",
-        email: "stokes.hubert@hotmail.com",
-        phone: "+1-580-977-4361",
-        company: "Bednar-Sawayn",
-        label: "Newsletter",
-    },
-    {
-        id: 3,
-        name: "Kaylee Taylor",
-        email: "diana45@hotmail.com",
-        phone: "+1-202-918-2132",
-        company: "Rolfson and Sons",
-        label: "Ads",
-    },
-    {
-        id: 4,
-        name: "Aiden Houston",
-        email: "ctromp@kassulke.info",
-        phone: "+1-215-480-3687",
-        company: "Wisoky, Windler and Nienow",
-        label: "Newsletter",
-    },
-    {
-        id: 5,
-        name: "Davis Houston",
-        email: "voreilly@yahoo.com",
-        phone: "+1-203-883-5460",
-        company: "Schmidt, Streich and Schuster",
-        label: "Ads",
-    },
-];
+// const customers: Customer[] = [
+//     {
+//         id: 1,
+//         name: "Lillian Carter",
+//         email: "xcollier@goodwin.com",
+//         phone: "+1-267-551-8666",
+//         company: "Larkin Group",
+//         label: "Marketing",
+//     },
+//     {
+//         id: 2,
+//         name: "Otto Walker",
+//         email: "stokes.hubert@hotmail.com",
+//         phone: "+1-580-977-4361",
+//         company: "Bednar-Sawayn",
+//         label: "Newsletter",
+//     },
+//     {
+//         id: 3,
+//         name: "Kaylee Taylor",
+//         email: "diana45@hotmail.com",
+//         phone: "+1-202-918-2132",
+//         company: "Rolfson and Sons",
+//         label: "Ads",
+//     },
+//     {
+//         id: 4,
+//         name: "Aiden Houston",
+//         email: "ctromp@kassulke.info",
+//         phone: "+1-215-480-3687",
+//         company: "Wisoky, Windler and Nienow",
+//         label: "Newsletter",
+//     },
+//     {
+//         id: 5,
+//         name: "Davis Houston",
+//         email: "voreilly@yahoo.com",
+//         phone: "+1-203-883-5460",
+//         company: "Schmidt, Streich and Schuster",
+//         label: "Ads",
+//     },
+// ];
 
 
 const ContentTable: React.FC = () => {
